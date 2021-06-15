352c39782c5d37d90089954612097e439371c940
JaelynSng <jaelyn.sng@gmail.com>
2021-06-15 18:13:24 +0800

    Added search bar, change marker and added list of cluster panel

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,14 +3,19 @@ class LocationsController < ApplicationController
     @locations = []
     clusters = Cluster.where.not(location_id: nil).where(is_active: true)
     clusters.each { |cluster| @locations << cluster.location }
-    
 
     @markers = @locations.map do |location|
-      {
-        lat: location.latitude,
-        lng: location.longitude,
-        info_window: render_to_string(partial: "info_window", locals: { location: location })
-      }
+      unless location.latitude.nil? and location.longitude.nil?
+        {
+          lat: location.latitude,
+          lng: location.longitude,
+          info_window: render_to_string(partial: "info_window", locals: { location: location }),
+          image_url: helpers.asset_url('bomb.png')
+        }
+      end
     end
+  
+
+    @all_clusters = Cluster.all
   end
 end
